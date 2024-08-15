32c7df1ed14cb1ad1aee7c63a2207e9e149c372f
Luciano Cardozo Casariego <lucianocardozocasariego@gmail.com>
2024-08-15 00:09:12 -0300

    ignote path in zipkin

--- Car-service/src/main/java/com/tutorial/Car_service/config/MicrometerCapability.java
+++ Car-service/src/main/java/com/tutorial/Car_service/config/MicrometerCapability.java
@@ -1,5 +1,8 @@
 package com.tutorial.Car_service.config;
 
+import brave.handler.MutableSpan;
+import brave.handler.SpanHandler;
+import brave.propagation.TraceContext;
 import feign.Capability;
 import io.micrometer.core.instrument.MeterRegistry;
 import org.springframework.context.annotation.Bean;
@@ -10,5 +13,19 @@ public class MicrometerCapability {
     @Bean
     public Capability capability(final MeterRegistry registry){
         return new feign.micrometer.MicrometerCapability(registry);
+    }
+
+    @Bean
+    public SpanHandler spanHandler() {
+        return new SpanHandler() {
+            public boolean end(TraceContext context, MutableSpan span, Cause cause) {
+                // Filtrar trazas no deseadas, como las de Eureka
+                String url = span.tag("http.url");
+                if (url != null && url.contains("/eureka/apps/")) {
+                    return false; // No enviar esta traza a Zipkin
+                }
+                return true; // Enviar otras trazas normalmente
+            }
+        };
     }
 }
